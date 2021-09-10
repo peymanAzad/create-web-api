c29f305de4c27d8f019e8471ddddd8ea5eba4aeb
peymanAzad <azad.peyman75@gmail.com>
2021-09-10 17:54:31 +0430

    success message and read me

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# create-web-api
-code generator for REST/GraphQL apis with JWT auth
+# create-graphql-api
+
+typescript REST/GraphqL api code generator with JWT authentication
+
+```
+npx create-web-api my-api
+cd my-api
+npm start
+```
+
+## Example
+
+you can see what this generator creates for you in [here](https://github.com/peymanAzad/web-api-template)
+
+## Used libraries
+
+### Common
+
+- [typescript](https://www.typescriptlang.org/)
+- [express](https://expressjs.com/)
+- [typeorm](https://typeorm.io/)
+- [argon2](https://github.com/ranisalt/node-argon2)
+- [json web token](https://github.com/auth0/node-jsonwebtoken)
+
+### Graphql
+
+- [apollo](https://www.apollographql.com/)
+- [type graphql](https://typegraphql.com/)
+- [class validator](https://github.com/typestack/class-validator)
+
+### REST api
+
+- [yup](https://github.com/jquense/yup)
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const inq = require("inquirer");
 const fs = require("fs-extra");
 const path = require("path");
 const { assert } = require("console");
+const chalk = require("chalk");
 const { questionAboutDatabase } = require("./questionAboutDatabase");
 const { questionInput } = require("./questionInput");
 const { copyCommonFiles } = require("./copyCommonfiles");
@@ -30,6 +31,8 @@ const main = async () => {
 	await copyCommonFiles(baseOutputPath, withGraphql, withRest);
 	if (withGraphql) await copyGraphqlFiles(baseOutputPath);
 	if (withRest) await copyRestFiles(baseOutputPath);
+
+	displaySuccesMessage(projectName, baseOutputPath);
 };
 
 const questionProjectName = async () => {
@@ -66,6 +69,30 @@ const copyRestFiles = async (baseOutputPath) => {
 	const outputPath = path.join(baseOutputPath, "/src/host/restApi");
 
 	await fs.copy(filesPath, outputPath);
+};
+
+const displaySuccesMessage = (projName, outputPath) => {
+	console.log(
+		`${chalk.green(
+			"Seccess!"
+		)} created ${projName} at ${outputPath}\nInside that directory run ${chalk.blue(
+			"'yarn' or 'npm i'"
+		)} for installing dependencies.\nThen you can run these commands:\n${chalk.blue(
+			"'yarn build' or 'npm run build'"
+		)}\n\tfor building typescript\n${chalk.blue(
+			"'yarn watch' or 'npm run watch'"
+		)}\n\tfor building and watching typescript files for changes\n${chalk.blue(
+			"'yarn start' or 'npm run start'"
+		)}\n\tfor starting project\n${chalk.blue(
+			"'yarn startd' or 'npm run startd'"
+		)}\n\tfor starting project with nodemon\n${chalk.blue(
+			"'yarn debug' or 'npm run debug'"
+		)}\n\tfor debuging\n${chalk.blue(
+			"'yarn create:migration' or 'npm run create:migration'"
+		)}\n\tfor creating migrations\nDon't forget to create database before running project\n${chalk.green(
+			"created with love!"
+		)}\n${chalk.green("happy coding!")}`
+	);
 };
 
 main();
